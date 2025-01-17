ee958e0ea2b8195ac2a8a1c42b9a826e0ed7cc76
LSV <simonne.visser@gmail.com>
2025-01-17 11:47:01 +0200

    Init

---
+++ firefox-draw.js
@@ -0,0 +1,11 @@
+import Editor from 'js-draw';
+import 'js-draw/styles';
+
+console.info('Loaded Firefox Draw');
+// console.info('Initializing editor...');
+
+// const editor = new Editor(document.body);
+
+// console.info('Editor initialized');
+
+// console.info(editor);
